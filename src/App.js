1838926723024fa92fc45f725ff8ee533da9784f
Ramiro Castagnaro <135472931+ramitagram@users.noreply.github.com>
2025-10-03 23:47:15 -0300

    Revert "Feat/services page"

--- src/App.js
+++ src/App.js
@@ -1,46 +1,24 @@
-import { Helmet } from 'react-helmet';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import BookingPage from './pages/BookingPage';
-import ServicesPage from './pages/ServicesPage';
-
-// Page Components
-import ContactPage from './pages/ContactPage';
-import HomePage from './pages/HomePage';
-import RoomDetailPage from './pages/RoomDetailPage';
-import RoomsPage from './pages/RoomsPage';
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-gray-100">
-        <Helmet>
-          <title>Hotel Hilton - Tu escapada de lujo</title>
-          <meta name="description" content="Disfruta de una experiencia única en Hotel Hilton" />
-        </Helmet>
-        
-        <Header />
-
-        <main className="flex-grow">
-          <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/habitaciones" element={<RoomsPage />} />
-          <Route path="/habitaciones/:id" element={<RoomDetailPage />}/>
-          <Route path="/reservar" element={<BookingPage />} />
-          <Route path="/contacto" element={<ContactPage />} />
-          <Route path="/servicios" element={<ServicesPage />} />
-          {/* Rutas adicionales para futuras paginas
-          <Route path="*" element={<NotFoundPage />} />
-          */}
-        </Routes>
-        </main>
-
-        <Footer />
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
